e0f4937414eb1f3b84ef7525e541d63bc2c1125a
lihong <lihongitchina@gmail.com>
2019-03-08 04:48:37 +0800

    update cmd flags desc

--- cmd/app.go
+++ cmd/app.go
@@ -9,14 +9,13 @@ import (
 
 func init() {
 	RootCmd.Flags().StringVarP(&tmpl, "tmpl", "t", "", "configuration template file(s) path")
-	RootCmd.Flags().StringVarP(&env, "env", "e", "", "configuration env, multi-env split by ','; default:dev")
-	RootCmd.Flags().StringVarP(&dist, "dist", "d", "", "generate dist path; default:config/")
-	RootCmd.Flags().StringVarP(&ptype, "ptype", "p", "json", "template parser type, support json,yaml,yml,toml; default:json")
+	RootCmd.Flags().StringVarP(&env, "env", "e", "dev", "configuration env, multi-env split by ','; eg. dev, prod, test")
+	RootCmd.Flags().StringVarP(&dist, "dist", "d", "config/", "generate dist path")
+	RootCmd.Flags().StringVarP(&ptype, "ptype", "p", "json", "template parser type; support-list: json, yaml, yml, toml")
 }
 
 func generate(cmd *cobra.Command, args []string) {
 	log.Println("generate comming")
-	log.Println("env")
 	lib.Show()
 	// Do Stuff Here
 }
