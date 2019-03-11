a0abbe4c2e74668a11c45121ccb9899c3b597b88
lihong <lihongitchina@gmail.com>
2019-03-11 10:07:18 +0800

    add version print

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"runtime"
 
 	"github.com/researchlab/confc/cmd"
 )
 
 func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+	fmt.Printf("confc %s (Git SHA: %s, Go Version: %s)\n", Version, GitSHA, runtime.Version())
 	if err := cmd.RootCmd.Execute(); err != nil {
 		log.Fatalln(err)
 	}
