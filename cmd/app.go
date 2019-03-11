e3b9a59ccc5735a4cb5e176d2bde31e5a616b8fb
lihong <lihongitchina@gmail.com>
2019-03-11 09:38:25 +0800

    update generate config function

--- cmd/app.go
+++ cmd/app.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"log"
-
 	"github.com/researchlab/confc/lib"
 	"github.com/spf13/cobra"
 )
@@ -12,10 +10,21 @@ func init() {
 	RootCmd.Flags().StringVarP(&env, "env", "e", "dev", "configuration env, multi-env split by ','; eg. dev, prod, test")
 	RootCmd.Flags().StringVarP(&dist, "dist", "d", "config/", "generate dist path")
 	RootCmd.Flags().StringVarP(&ptype, "ptype", "p", "json", "template parser type; support-list: json, yaml, yml, toml")
+	RootCmd.Flags().StringVarP(&cache, "cache", "c", "on", "cache cmdline parameters into the system environment variable; on: cache, off: no cache, flush: flush cache")
 }
 
-func generate(cmd *cobra.Command, args []string) {
-	log.Println("generate comming")
-	lib.Show()
+func genConf(cmd *cobra.Command, args []string) error {
+	var genConfCtx = &lib.GenConfCtx{
+		Tmpl:  tmpl,
+		Env:   env,
+		Dist:  dist,
+		Ptype: ptype,
+		Cache: cache,
+	}
+	err := lib.Assembly(genConfCtx)
+	if err != nil {
+		return err
+	}
+	return nil
 	// Do Stuff Here
 }
