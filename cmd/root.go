1ee927ee5a014a23d5ded330993f69bdf68d4434
lihong <lihongitchina@gmail.com>
2019-03-11 10:07:06 +0800

    add version print

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,7 @@ var RootCmd = &cobra.Command{
 	Use:   "confc-cli",
 	Short: "confc-cli is is a lightweight configuration management tool.",
 	Long: `A tool which keeping local configuration files up-to-date using data stored in each env config file itself;
-	              is really convenience for multi-env config file generate.
+	              and is really convenience for multi-env configuration files generate.
                 `,
 	RunE: genConf,
 }
