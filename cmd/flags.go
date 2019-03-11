7189553749075ac8568881ad581fa06142ab8cdb
lihong <lihongitchina@gmail.com>
2019-03-11 09:38:40 +0800

    update cache flag

--- cmd/flags.go
+++ cmd/flags.go
@@ -5,4 +5,5 @@ var (
 	env   string //config env, multi-env split by ',' default: dev
 	dist  string //config generate path; default: ./config
 	ptype string //template file parser type, support json,yaml,yml,toml, default:json
+	cache string //cache cmdline parameters into the system environment variable; on: cache, off: no cache, flush: flush cache
 )
