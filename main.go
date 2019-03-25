722b2748c986edc61d79aad1d5acb3e959ed6a9e
lihong <lihongitchina@gmail.com>
2019-03-25 14:21:55 +0800

    logging

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+	log.SetFlags(log.Ldate | log.Ltime) //| log.Llongfile)
 	log.Printf("confc %s (Git SHA: %s, Go Version: %s)\n", Version, GitSHA, runtime.Version())
 	if err := cmd.RootCmd.Execute(); err != nil {
 		log.Fatalln(err)
